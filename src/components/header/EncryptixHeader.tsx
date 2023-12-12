d8ec57e8e4793bead23edaa8dffda20564d76423
Dustyn <dustyn_gibb@hotmail.com>
2023-12-12 12:42:51 -0500

    intiial commit

---
+++ src/components/header/EncryptixHeader.tsx
@@ -0,0 +1,11 @@
+const EncryptixHeader = () => {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" component="div">
+          Encryptix
+        </Typography>
+      </Toolbar>
+    </AppBar>
+  );
+};
