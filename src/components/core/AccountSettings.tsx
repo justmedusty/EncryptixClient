cbb4796db3cd4a135a9e0507c3a89ba7ee82f194
Dustyn <dustyn_gibb@hotmail.com>
2024-02-24 08:22:38 -0500

    changed logo and app name, need to fix issue with text field in account settings component apparently re-fucking-rendering with every input change (what it seems like anyway) will continue to investigate

--- src/components/core/AccountSettings.tsx
+++ src/components/core/AccountSettings.tsx
@@ -1,32 +1,34 @@
-import React, {useEffect, useState} from 'react';
+import React, {useEffect, useRef, useState} from 'react';
 import {Alert, Box, Button, Container, Grid, Paper, Snackbar, Tab, Tabs, TextField, Typography} from '@mui/material';
 import {getToken} from "../../auth/TokenStorage";
 import enums from "../../enums/enums";
 import {wait} from "@testing-library/user-event/dist/utils";
 
 
-
-
-
-
 const AccountSettings: React.FC = () => {
-    const [success,setSuccess] = useState(false)
-    const [failure,setFailure] = useState(false)
+    const [success, setSuccess] = useState(false)
+    const [failure, setFailure] = useState(false)
     const [newUser, setNewUser] = useState('');
     const [newPassword, setNewPassword] = useState('');
     const [publicKey, setNewPublicKey] = useState('');
+    //const [selectionStart, setSelectionStart] = useState(0);
+    //const [selectionEnd, setSelectionEnd] = useState(0);
 
-    const resetFailure = async() => {
-            await wait(3000)
-            setFailure(false)
+
+    const resetFailure = async () => {
+        await wait(3000)
+        setFailure(false)
     }
-    const resetSuccess = async() => {
-            await wait(3000)
-            setSuccess(false)
+    const resetSuccess = async () => {
+        await wait(3000)
+        setSuccess(false)
     }
 
+    const [tabValue, setTabValue] = useState(0);
 
-
+    const handleChange = (event: any, newValue: React.SetStateAction<number>) => {
+        setTabValue(newValue);
+    };
 
     interface UpdateUsernameFormProps {
         onSubmit: (data: { newUser: string }) => void;
@@ -42,35 +44,34 @@ const AccountSettings: React.FC = () => {
 
         };
 
-        return (
-            <form onSubmit={handleSubmit}>
-                <Snackbar open={failure} autoHideDuration={4000} anchorOrigin={{vertical:'top', horizontal:'center'}}>
-                    <Alert severity="error">Username changed failed, must be unique and at least 6 chars</Alert>
-                </Snackbar>
-                <Snackbar open={success} autoHideDuration={4000} anchorOrigin={{vertical:'top', horizontal:'center'}}>
-                    <Alert severity="success">Username Changed!</Alert>
-                </Snackbar>
+        return (<form onSubmit={handleSubmit}>
+            <Snackbar open={failure} autoHideDuration={4000} anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert severity="error">Username changed failed, must be unique and at least 6 chars</Alert>
+            </Snackbar>
+            <Snackbar open={success} autoHideDuration={4000} anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert severity="success">Username Changed!</Alert>
+            </Snackbar>
 
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            fullWidth
-                            label="New Username"
-                            value={newUser}
-                            onChange={(e) => setNewUser(e.target.value)}
-                            required
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button type="submit" variant="contained" color="primary" fullWidth>
-                            Update Username
-                        </Button>
-                    </Grid>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        fullWidth
+                        label="New Username"
+                        value={newUser}
+                        onChange={(e) => setNewUser(e.target.value)}
+                        required
+                    />
                 </Grid>
-            </form>
-        );
+                <Grid item xs={12}>
+                    <Button type="submit" variant="contained" color="primary" fullWidth>
+                        Update Username
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>);
     };
+
 
     interface UpdatePasswordFormProps {
         onSubmit: (data: { newPassword: string }) => void;
@@ -86,36 +87,87 @@ const AccountSettings: React.FC = () => {
         };
 
 
-
-        return (
-            <form onSubmit={handleSubmit}>
-                <Snackbar open={failure} autoHideDuration={4000} anchorOrigin={{vertical:'top', horizontal:'center'}}>
-                    <Alert severity="error">Username changed failed, must be unique and at least 6 chars</Alert>
-                </Snackbar>
-                <Snackbar open={success} autoHideDuration={4000} anchorOrigin={{vertical:'top', horizontal:'center'}}>
-                    <Alert severity="success">Username Changed!</Alert>
-                </Snackbar>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            fullWidth
-                            type="password"
-                            label="New Password"
-                            value={newPassword}
-                            onChange={(e) => setNewPassword(e.target.value)}
-                            required
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button type="submit" variant="contained" color="primary" fullWidth>
-                            Update Password
-                        </Button>
-                    </Grid>
+        return (<form onSubmit={handleSubmit}>
+            <Snackbar open={failure} autoHideDuration={4000} anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert severity="error">Password change failed, must be unique and at least 8 chars</Alert>
+            </Snackbar>
+            <Snackbar open={success} autoHideDuration={4000} anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert severity="success">Password Changed!</Alert>
+            </Snackbar>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        fullWidth
+                        type="password"
+                        label="New Password"
+                        value={newPassword}
+                        onChange={(e) => setNewPassword(e.target.value)}
+                        required
+                    />
                 </Grid>
-            </form>
-        );
+                <Grid item xs={12}>
+                    <Button type="submit" variant="contained" color="primary" fullWidth>
+                        Update Password
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>);
     };
+    const handleUsernameSubmit = async ({newUser}: { newUser: string }): Promise<void> => {
+        try {
+            const token: string | null = getToken();
+            const formData: URLSearchParams = new URLSearchParams();
+            formData.append('newUser', newUser);
+
+
+            const response = await fetch(enums.URL + enums.PORT + enums.CHANGE_USERNAME, {
+                method: 'POST', headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded', Authorization: `Bearer ${token}`,
+                }, body: formData,
+            });
+
+            if (!response.ok) {
+                setFailure(true)
+                await resetFailure()
+            } else if (response.ok) {
+                setSuccess(true)
+                await resetSuccess()
+            }
+
+        } catch (error) {
+            console.error('Error updating username:', error);
+            alert('Error updating username')
+        }
+    };
+
+    const handlePasswordSubmit = async ({newPassword}: { newPassword: string }): Promise<void> => {
+        try {
+            const token = getToken();
+            const formData = new URLSearchParams();
+            formData.append('newPassword', newPassword);
+
+            const response = await fetch(enums.URL + enums.PORT + enums.CHANGE_PASSWORD, {
+                method: 'POST', headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded', Authorization: `Bearer ${token}`,
+                }, body: formData,
+            });
+
+            if (!response.ok) {
+                setFailure(true)
+                await resetFailure()
+
+            } else if (response.ok) {
+                setSuccess(true)
+                await resetSuccess()
+            }
+
+        } catch (error) {
+            console.error('Error updating username:', error);
+            alert('Error updating username')
+        }
+    };
+
 
     interface UpdatePublicKeyFormProps {
         onSubmit: (data: { publicKey: string }) => void;
@@ -124,105 +176,39 @@ const AccountSettings: React.FC = () => {
     const UpdatePublicKeyForm: React.FC<UpdatePublicKeyFormProps> = ({onSubmit}) => {
 
 
-
         const handleSubmit = async (e: React.FormEvent) => {
-            e.preventDefault();
             onSubmit({publicKey: publicKey});
             setNewPublicKey('')
         };
 
-        return (
-            <form onSubmit={handleSubmit}>
-                <Snackbar open={failure} autoHideDuration={4000} anchorOrigin={{vertical:'top', horizontal:'center'}}>
-                    <Alert severity="error">Public key upload failed, check format </Alert>
-                </Snackbar>
-                <Snackbar open={success} autoHideDuration={4000} anchorOrigin={{vertical:'top', horizontal:'center'}}>
-                    <Alert severity="success">Public key changed!</Alert>
-                </Snackbar>
-                <Grid container spacing={2}>
-                    <Grid item xs={12}>
-                        <TextField
-                            variant="outlined"
-                            fullWidth
-                            label="New Public Key"
-                            multiline
-                            rows={4}
-                            value={publicKey}
-                            onChange={(e) => setNewPublicKey(e.target.value)}
-                            required
-                        />
-                    </Grid>
-                    <Grid item xs={12}>
-                        <Button type="submit" variant="contained" color="primary" fullWidth>
-                            Update Public Key
-                        </Button>
-                    </Grid>
+        return (<form onSubmit={handleSubmit}>
+            <Snackbar open={failure} autoHideDuration={4000} anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert severity="error">Public key upload failed, check format </Alert>
+            </Snackbar>
+            <Snackbar open={success} autoHideDuration={4000} anchorOrigin={{vertical: 'top', horizontal: 'center'}}>
+                <Alert severity="success">Public key changed!</Alert>
+            </Snackbar>
+            <Grid container spacing={2}>
+                <Grid item xs={12}>
+                    <TextField
+                        variant="outlined"
+                        fullWidth
+                        label="New Public Key"
+                        multiline
+                        rows={4}
+                        value={publicKey}
+                        onChange={(e) => setNewPublicKey(e.target.value)}
+                        required
+                    />
                 </Grid>
-            </form>
-        );
+                <Grid item xs={12}>
+                    <Button type="submit" variant="contained" color="primary" fullWidth>
+                        Update Public Key
+                    </Button>
+                </Grid>
+            </Grid>
+        </form>);
     };
-    const handlePasswordSubmit= async ({newPassword}: { newPassword: string }): Promise<void> => {
-        try {
-            const token = getToken();
-            const formData = new URLSearchParams();
-            formData.append('newPassword', newPassword);
-
-            const response = await fetch(enums.URL + enums.PORT + enums.CHANGE_PASSWORD, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    Authorization: `Bearer ${token}`,
-                },
-                body: formData,
-            });
-
-            if (!response.ok) {
-                setFailure(true)
-                resetFailure()
-
-            }
-            else if (response.ok){
-                setSuccess(true)
-                resetSuccess()
-            }
-
-        } catch (error) {
-            console.error('Error updating username:', error);
-            alert('Error updating username')
-        }
-    };
-
-
-    const handleUsernameSubmit = async ({newUser}: { newUser: string }): Promise<void> => {
-        try {
-            const token = getToken();
-            const formData = new URLSearchParams();
-            formData.append('newUser', newUser);
-
-            const response = await fetch(enums.URL + enums.PORT + enums.CHANGE_USERNAME, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    Authorization: `Bearer ${token}`,
-                },
-                body: formData,
-            });
-
-            if (!response.ok) {
-                setFailure(true)
-                resetFailure()
-            }
-            else if (response.ok){
-                setSuccess(true)
-                resetSuccess()
-            }
-
-        } catch (error) {
-            console.error('Error updating username:', error);
-            alert('Error updating username')
-        }
-    };
-
 
     const handlePublicKeySubmit = async ({publicKey}: { publicKey: string }): Promise<void> => {
         try {
@@ -231,33 +217,25 @@ const AccountSettings: React.FC = () => {
             formData.append('publicKey', publicKey);
 
             const response = await fetch(enums.URL + enums.PORT + enums.UPLOAD_NEW_KEY, {
-                method: 'POST',
-                headers: {
-                    'Content-Type': 'application/x-www-form-urlencoded',
-                    Authorization: `Bearer ${token}`,
-                },
-                body: formData,
+                method: 'POST', headers: {
+                    'Content-Type': 'application/x-www-form-urlencoded', Authorization: `Bearer ${token}`,
+                }, body: formData,
             });
 
             if (!response.ok) {
                 setFailure(true)
-                resetFailure()
+                await resetFailure()
 
-            }
-            else if (response.ok){
+            } else if (response.ok) {
                 setSuccess(true)
-                resetSuccess()
+                await resetSuccess()
             }
 
         } catch (error) {
             console.error('Error updating public key:', error);
         }
     };
-    const [tabValue, setTabValue] = useState(0);
 
-    const handleChange = (event: any, newValue: React.SetStateAction<number>) => {
-        setTabValue(newValue);
-    };
 
     const ViewPublicKey: React.FunctionComponent = () => {
         const [publicKey, setPublicKey] = useState("");
@@ -267,10 +245,8 @@ const AccountSettings: React.FC = () => {
             const fetchData = async () => {
                 try {
                     const response = await fetch(enums.URL + enums.PORT + enums.GET_MY_KEY, {
-                        method: 'GET',
-                        headers: {
-                            'Content-Type': 'application/json',
-                            Authorization: `Bearer ${token}`,
+                        method: 'GET', headers: {
+                            'Content-Type': 'application/json', Authorization: `Bearer ${token}`,
                         },
                     });
                     if (!response.ok) {
@@ -291,12 +267,9 @@ const AccountSettings: React.FC = () => {
         }, [token]);
 
 
-        return (
-            <Container>
-
-                <Typography sx={{textAlign: 'center'}}>{publicKey}</Typography>
-            </Container>
-        )
+        return (<Container>
+            <Typography sx={{textAlign: 'center'}}>{publicKey}</Typography>
+        </Container>)
     }
 //This doesn't work in the backend for some reason, need to fix but don't feel like it right now since not super important
     /*
@@ -357,8 +330,7 @@ const AccountSettings: React.FC = () => {
                     <Grid item xs={12}>
                         <Paper elevation={3} style={{padding: 16}}>
                             <Typography variant="h5" sx={{
-                                display: 'flex',
-                                justifyContent: 'center'
+                                display: 'flex', justifyContent: 'center'
                             }}>{tabValue === 0 && 'Update Username'}
                                 {tabValue === 1 && 'Update Password'}
                                 {tabValue === 2 && 'Update Public Key'}
@@ -372,8 +344,7 @@ const AccountSettings: React.FC = () => {
                     </Grid>
                 </Grid>
             </Paper>
-        </Container>
-    );
+        </Container>);
 };
 
 export default AccountSettings;
